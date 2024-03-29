8b06547bb11dbdc473c00c335166093fa8475057
Prakhar Soni <prakhar0912@gmail.com>
2024-03-29 09:21:53 +0530

    done

--- dbv.py
+++ dbv.py
@@ -4,11 +4,17 @@ users = ["U16","U16","U32","U52","U62","U62","U62","U62","UX6","UX6","UX6","D21"
 hostnames = ["peplap07963.pi.pvt","peplap07968.pi.pvt","pepldr03117.pi.pvt","peplap07896.pi.pvt","peplap09459.pi.pvt","peplap09458.pi.pvt","peplap09489.pi.pvt","peplap09488.pi.pvt","peplgp01316.pi.pvt","t01lap03580.pi.pvt","peplap09587.pi.pvt","peplap07883.pi.pvt","pepldr03037.pi.pvt","peplap07410.pi.pvt","peplap09067.pi.pvt","peplgp01254.pi.pvt","peplap08701.pi.pvt","pepldr02966.pi.pvt","peplap07491.pi.pvt","peplap08704.pi.pvt","peplgp01273.pi.pvt","peplap07967.pi.pvt","pepldr03113.pi.pvt","peplap07428.pi.pvt","peplap09224.pi.pvt","peplgp01255.pi.pvt","peplap09584.pi.pvt","peplap09230.pi.pvt","peplap09172.pi.pvt","peplap09173.pi.pvt","peplap09202.pi.pvt","peplap09185.pi.pvt","peplap09066.pi.pvt","peplap08703.pi.pvt"]
 outputs = []
 
-f = open("dbVersion.txt", "a")
+f = open("dbVersionFinal.txt", "a")
+
+
+
 
 for i,user in enumerate(users):
-    try: 
-        output = subprocess.check_output("pbrun -u {}adm -h {} bash -c '{}'".format(user.lower(), hostnames[i], 'sqlplus -v | grep -Po "(?<=Version )[^ ]+"'), shell=True)
+    try:
+        sqlCommand = '''pbrun -u {}adm -h {} SHELL <<"ENDPBRUN"
+sqlplus -v | grep -Po "(?<=Version )[^ ]+"
+ENDPBRUN'''.format(user.lower(), hostnames[i])
+        output = subprocess.check_output(sqlCommand, shell=True)
     except:
         try: 
             output = subprocess.check_output("pbrun -u {}adm -h {} bash -c '{}'".format(user.lower(), hostnames[i], 'hdbsql -v | grep -Po "(?<=version )[^,]+"'), shell=True)
